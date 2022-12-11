49de09743fb16970b7a0f6a3f17108e5a28b2f86
wangxiaogang0629 <346775171@qq.com>
2022-12-11 17:05:34 +0800

    feat: remake

--- src/drawRedRect.js
+++ src/drawRedRect.js
@@ -1,9 +1,10 @@
+// ceshi
 export function drawRedRect(svg) {
   const rect = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
   rect.setAttribute('x', 0);
   rect.setAttribute('y', 0);
   rect.setAttribute('fill', 'red');
-  rect.setAttribute('width', 100);
-  rect.setAttribute('height', 100);
+  rect.setAttribute('width', 200);
+  rect.setAttribute('height', 200);
   svg.appendChild(rect);
 }
